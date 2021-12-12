5da3af755bb2ff3324b8d80c4128624f117d89cd
Jacob Morris <jacob.morris1410@gmail.com>
2021-12-12 02:32:07 -0500

    Day 1 Solution 2 in C

---
+++ day01/solution2.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+
+/**
+  * This function reads the given file and determines how many relevant pieces of data are in it.
+  * In this case, every line in the file contains 1 piece of data.
+  */
+int getSizeOfData(char* filename) {
+    FILE* fp = fopen(filename, "r");
+    // file could not be read
+    if (fp == NULL) {
+        printf("File could not be read.\n");
+        return -1;
+    }
+
+    // iterate over file and return the number of pieces of data
+    int nData = 0;
+    while (!feof(fp)) {
+        char ch = fgetc(fp);
+        if (ch == '\n') {
+            nData++;
+        }
+    }
+    return nData;
+}
+
+int readInput(char* filename, int* data) {
+    FILE* fp = fopen(filename, "r");
+    // file could not be read
+    if (fp == NULL) {
+        printf("File could not be read.\n");
+        return -1;
+    }
+
+    int i = 1;
+    int val;
+    // read in data as an int
+    while (fscanf(fp, "%d", &val) == 1) {
+        printf("%d\n", val);
+        data[i++] = val;
+        //printf("i: %d, val: %d\n", i-1, data[i-1]);
+    }
+    return 0;
+}
+
+/**
+  * Iterates over the data and counts the number of times it has increased.
+  * windowSize determines the size of the sliding window which we compute the sum under.
+  */
+int timesIncreased(int* data, int windowSize) {
+    // length of array is stored as metadata in the array
+    int n = data[0];
+    printf("Iterating over %d items\n", n);
+
+    int n_increased = 0;
+    // get initial value
+    int last_val = 0;
+    for (int i = 1; i <= windowSize; i++) {
+        last_val += data[i];
+    }
+    printf("sum: %d\n", last_val);
+
+    for (int i = 2; i < n - 1; i++) {
+        int curr_val = 0;
+        for (int j = 0; j < windowSize; j++) {
+            curr_val += data[i + j];
+        }
+        printf("sum: %d\n", curr_val);
+        // check if we increased depth
+        if (curr_val > last_val) {
+            printf("%d > %d\n", curr_val, last_val);
+            n_increased++;
+        }
+        last_val = curr_val;
+    }
+    return n_increased;
+}
+
+
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        printf("Usage: ./s2 filename n\n");
+        printf("n is the sliding window size.\n");
+        exit(0);
+    }
+    const int WINDOWSIZE = atoi(argv[2]);
+
+    // get number of ints in file
+    int nData = getSizeOfData(argv[1]);
+    printf("Number of lines in file: %d\n", nData);
+
+    // first item in this list is the length of the array
+    int data[nData + 1];
+    data[0] = nData;
+
+    readInput(argv[1], data);
+    printf("%d\n", data[3]);
+
+    int answer = timesIncreased(data, WINDOWSIZE);
+    printf("The answer is %d\n", answer);
+
+    return 0;
+}
+
